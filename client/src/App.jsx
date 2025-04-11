3967d2be21f36a15180f70befeebbe5a011bfd40
cath <catherine.george.irimben@gmail.com>
2025-04-11 10:56:18 +0530

    fixed-imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './routes/Home/Home';
+import Home from './routes/Home/home';
 import About from './routes/About/about';
 import Header from './components/Header';
 import Footer from './components/Footer';
